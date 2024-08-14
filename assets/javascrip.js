efccfe3dc55d4bdbe8b5674b9ab2ed7912533c5f
Wil220 <wilker.silva0714@gmail.com>
2024-08-14 18:16:14 -0300

    Loja apple

--- assets/javascrip.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Adiciona um evento ao botão de envio para mostrar uma mensagem
-    const formButton = document.querySelector('form button');
-    formButton.addEventListener('click', function(event) {
-        event.preventDefault();
-        alert('Obrigado por entrar em contato!');
-    });
-});
